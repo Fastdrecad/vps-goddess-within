91d65865765081af392ef0def9c6e28d1e758f54
Fastdrecad <andrijas.micun@gmail.com>
2024-09-21 06:56:16 +0200

    Add GitHub Actions workflow for auto deployment

--- server/utils/db.js
+++ server/utils/db.js
@@ -1,15 +1,16 @@
-require('dotenv').config();
-const chalk = require('chalk');
-const mongoose = require('mongoose');
-
-const keys = require('../config/keys');
-const { database } = keys;
+require("dotenv").config();
+const chalk = require("chalk");
+const mongoose = require("mongoose");
 
 const setupDB = async () => {
   try {
-    // Connect to MongoDB
-    await mongoose.connect(database.url);
-    console.log(`${chalk.green('✓')} ${chalk.blue('MongoDB Connected!')}`);
+    const mongoURI = process.env.MONGO_URI;
+    if (!mongoURI) {
+      throw new Error("MONGO_URI is not defined in .env file");
+    }
+    console.log("MONGO_URI:", mongoURI);
+    await mongoose.connect(mongoURI);
+    console.log(`${chalk.green("✓")} ${chalk.blue("MongoDB Connected!")}`);
   } catch (error) {
     console.log(`Error: ${error.message}`);
     process.exit(1);
